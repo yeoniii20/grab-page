5363f6ada674e7adb8699ebc3bbf274755854803
yoeniii20 <commetoi_yeoni@naver.com>
2022-05-12 13:51:31 +0900

    Link 태그 적용

--- src/App.js
+++ src/App.js
@@ -1,10 +1,40 @@
 import "./App.css";
 import MainPageComponent from "./main";
-import ChildComponent from "./child.js";
-import TimerComponent from "./timer";
+import { Switch, Route } from "react-router-dom";
+import UploadPage from "./upload";
+import ProductPage from "./product";
 
 function App() {
-  return <MainPageComponent />;
+  return (
+    <div>
+      <Switch>
+        <Route exact={true} path="/">
+          <MainPageComponent />
+        </Route>
+        <Route exact={true} path="/products/:id">
+          <ProductPage />
+        </Route>
+        <Route exact={true} path="/upload">
+          <UploadPage />
+        </Route>
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
+
+//이것도 가능함
+//function App() {
+//return (
+// <div>
+// <Switch>
+//  <Route path="/" exact component={MainPageComponent} />
+//  <Route path="/product" component={ProductPage} />
+//  <Route path="/upload" component={UploadPage} />
+// </Switch>
+//</div>
+// );
+//}
+
+//export default App;
